903eed553a960f0ab45ab583bc312570d0cd7bbf
JacobForsman <jaaforsman@gmail.com>
2019-08-20 13:26:06 +0300

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+let input = document.getElementById('name');
+let button = document.getElementById('button');
+
+button.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+   alert ("Hello " + input.value);}
